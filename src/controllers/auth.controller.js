0ba3a945d69f134ce106cf636f18377765f61d2f
Harsh_Narayan <harshnarayannishad@gmail.com>
2025-07-22 15:05:24 +0530

    Authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import Users from '../models/Users.js';
-import Doctors from '../models/Doctors.js';
+import Users from '../models/Users.model.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
